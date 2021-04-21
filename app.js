d967685bfbafcf8dba0096675c5de8878c01d424
Esteban Ordonez <estebanusco@gmail.com>
2021-04-20 22:13:26 -0700

    implement send messages to users

--- app.js
+++ app.js
@@ -7,29 +7,45 @@ const fetchPosts = async () => {
   try {
     const response = await fetch(`${BASE_URL}/posts`);
     const data = await response.json();
-
+    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
   }
 };
 
-const renderPost = (posts) => {
+const fetchUserData = async () => {
+  const token = fetchToken();
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log(result.data);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const renderPost = (userData, posts) => {
   $(".cards-div").empty();
   posts.forEach((post) => {
-    const postElem = createPostHtml(post);
+    const postElem = createPostHtml(userData, post);
     $(".cards-div").append(postElem);
   });
 };
 
-function createPostHtml(post) {
+function createPostHtml(userData, post) {
   const {
-    _id,
     title,
     description,
     price,
-    author: { username },
+    author: { username, _id },
   } = post;
+
   //   data-id=${_id}
   return $(`
     <div class="card" >
@@ -39,10 +55,14 @@ function createPostHtml(post) {
           <p class="card-text">${price}</p>
           <p class="card-text"><b>${username}</b></p>
           
-          <button id="btn-delete" class="btn btn-primary">Delete</button>
-          <button id="btn-edit" class="btn btn-primary">Edit</button>
+            ${
+              userData.data._id === _id
+                ? `<button id="btn-delete" class="btn btn-primary">Delete</button>
+                    <button id="btn-edit" class="btn btn-primary">Edit</button>`
+                : '<button id="btn-message" class="btn btn-primary">Send a Message</button> '
+            }
         </div>
-  </div>  `).data("post", post);
+  </div> `).data("post", post);
 }
 
 const registerUser = async (userName, password) => {
@@ -92,6 +112,7 @@ const loginUser = async (userName, password) => {
     localStorage.setItem("token", JSON.stringify(token));
     hideRegistration();
   } catch (error) {
+    console.log(error.message);
     throw error;
   }
 };
@@ -120,20 +141,25 @@ const hideRegistration = () => {
 
   if (token) {
     $("#registration").css("display", "none");
-    $(".cards-div").css("visibility", "visible");
+    $("#post-form").css("visibility", "visible");
+    $("#post-form").removeClass("disabled");
     fetchAndRender();
   } else {
-    $(".cards-div").css("visibility", "hidden");
+    $("#post-form").css("visibility", "hidden");
+    $("#post-form").addClass("disabled");
+    fetchAndRender();
   }
 };
 
 const fetchAndRender = async () => {
   const posts = await fetchPosts();
-  renderPost(posts.data.posts);
+  const userData = await fetchUserData();
+
+  renderPost(userData, posts.data.posts);
 };
 
 const createPost = async (postObject) => {
-  console.log(postObject);
+  //console.log(postObject);
   const token = fetchToken();
 
   if (token) {
@@ -152,14 +178,16 @@ const createPost = async (postObject) => {
       console.log(error);
       throw error;
     }
+  } else {
+    //console.log("please log in or register")
   }
 };
 
 $("#post-form").on("submit", async (event) => {
   event.preventDefault();
 
-  const {card, postElem} = $("#post-form").data();
-  console.log(card, postElem);
+  const { card, postElem } = $("#post-form").data();
+  //console.log(card, postElem);
 
   const postData = {
     post: {
@@ -169,50 +197,47 @@ $("#post-form").on("submit", async (event) => {
     },
   };
 
-  if (card) {
-    //editing
-    console.log(card, "editing")
-    try{
-        const result = await updatePost(card.post._id, postData )
-        console.log('result is:' , result )
-        fetchAndRender()
-        $('#post-form').data({ card: null, postElem: null })
-        $('#post-form').trigger('reset')
-    }catch(error){
-        console.log(error)
-    }
-  } else {
-    // new post
-    try {
+  try {
+    if (card) {
+      //editing
+      console.log(card, "editing");
+
+      const result = await updatePost(card.post._id, postData);
+      console.log("result is:", result);
+      fetchAndRender();
+      $("#post-form").data({ card: null, postElem: null });
+      $("#post-form").trigger("reset");
+    } else {
+      // new post
       await createPost(postData);
       fetchAndRender();
       $(".row").trigger("reset");
-    } catch (error) {
-      throw error;
     }
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 });
 
 const updatePost = async (postId, updatedPost) => {
-    const token = fetchToken();
-    try{
-        const response = await fetch(`${BASE_URL}/posts/${postId}`,{
-        method : "PATCH",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(updatedPost)
-        });
-        const result = await response.json();
-        console.log(result)
-        return result;
-    } catch (error){
-        console.log(error)
-        throw error
-    }
-}
-
+  const token = fetchToken();
+  try {
+    const response = await fetch(`${BASE_URL}/posts/${postId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(updatedPost),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 const deletePost = async (postId) => {
   const token = fetchToken();
@@ -227,7 +252,7 @@ const deletePost = async (postId) => {
         },
       });
       const result = await response.json();
-      //console.log(result);
+      console.log(result);
     } catch (error) {
       console.log(error);
       throw error;
@@ -241,7 +266,7 @@ $(".cards-div").on("click", async (event) => {
   const parentElem = $(event.target).parent();
   const card = parentElem.parent().data();
   const postElem = parentElem.parent();
-//   console.log(card, postElem);
+  //   console.log(card, postElem);
   //const { post: { _id }} = card;
   //console.log(_id);
 
@@ -256,16 +281,80 @@ $(".cards-div").on("click", async (event) => {
     }
   } else if (idBtn === "btn-edit") {
     $("#post-form").data({ card, postElem });
-    const data = $("#post-form").data()
-    console.log(data)
+    //const data = $("#post-form").data(); //erase lines
+    //console.log(data); //erase
     $("#post-title").val(card.post.title);
     $("#post-body").val(card.post.description);
     $("#post-price").val(card.post.price);
+  } else if (idBtn === "btn-message") {
+    $("#message-form").data({ card });
+    //const data = $("#message-form").data(); //erase
+    //console.log(data); //erase
+    $(".modal").addClass("open");
+    $(".message-label")
+      .text(`Your message to: ${card.post.author.username}`)
+      .css("font-weight", "Bold");
+
+    $(".card-title").text(`${card.post.title}`);
+    $(".card-desc").text(`${card.post.description}`);
+    $(".card-price").text(`${card.post.price}`);
   } else {
     return;
   }
 });
 
+$("#cancel-messageBtn").on("click", () => {
+  $(".modal").removeClass("open");
+});
+
+$("#message-form").on("submit", async (event) => {
+  event.preventDefault();
+  const messageData = {
+    message: {
+      content: $("#message-body").val(),
+    },
+  };
+  try {
+    const result = await sendMessage(messageData);
+    console.log(result);
+    $(".modal").removeClass("open");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+});
+
+const sendMessage = async (messageData) => {
+  const token = fetchToken();
+  try {
+    const response = await fetch(
+      `${BASE_URL}/posts/607e0b4c2482a300177065c6/messages`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(messageData),
+      }
+    );
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// $("#btn-message").on("click", (event)=> {
+// const parentElem = $(event.target).parent()
+// console.log(parentElem)
+// const card = parentElem.parent().data();
+// console.log(card)
+// });
+
 (async () => {
   hideRegistration();
+  //sendMessage()//test call
 })();
